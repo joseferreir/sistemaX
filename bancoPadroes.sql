87de42e1277bb1b2a576cb2e5fdf1ddde514f63e
jose <joseferreiravieira123@gmail.com>
2016-02-13 17:07:51 -0200

    script do BD

---
+++ bancoPadroes.sql
@@ -0,0 +1,21 @@
+﻿
+ CREATE TABLE Usuario(
+	matricula VARCHAR(6) NOT NULL,
+	nomeUsuario VARCHAR(50) NOT NULL,
+	email VARCHAR(60) UNIQUE NOT NULL,
+	senha VARCHAR(16) NOT NULL UNIQUE, 
+	foto VARCHAR(100) NOT NULL,
+	papel VARCHAR(35) NOT NULL,
+	status BOOLEAN  DEFAULT TRUE, 
+	PRIMARY KEY (matricula) 
+
+);
+
+CREATE TABLE Feriado(
+	data DATE NOT NULL,
+	nome VARCHAR(30) NOT NULL UNIQUE,
+	PRIMARY KEY (nome,data)
+);
+-- Usuário padrão
+
+INSERT INTO usuario (matricula , nomeUsuario, email,senha,foto, papel) VALUES('000000', 'admin', 'admin@gmail.com', 'admin153Chg%','jose2.jpg' ,'ADMISTRAD0R');
